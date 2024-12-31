f51b9b8ea4335d34e90c02c59e592668c0cf427b
Pranay K R <2005pranaykr@gmail.com>
2024-12-31 16:24:23 +0530

    changing meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Pranay - Portfolios',
+  title: 'Pranay - Portfolio',
   description: 'Pranay50X Portfolio',
 }
 
